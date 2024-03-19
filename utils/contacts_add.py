3d59b2185e4f6946360487dad724cb932c2e062a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-19 14:34:56 -0300

    utils project contacts

---
+++ utils/contacts_add.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+from random import choice
+
+import django
+from django.conf import settings
+
+DJANGO_BASE_DIR = Path(__file__).parent.parent
+NUMBER_OF_OBJECTS = 1000
+
+sys.path.append(str(DJANGO_BASE_DIR))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+settings.USE_TZ = False
+
+django.setup()
+
+if __name__ == '__main__':
+    import faker
+
+    from home.models import Home
+
+    Home.objects.all().delete()
+
+    fake = faker.Faker('pt_BR')
+
+    django_contacts = []
+
+    for _ in range(NUMBER_OF_OBJECTS):
+        profile = fake.profile()
+        email = profile['mail']
+        username= profile['name']
+        password = fake.phone_number()
+        created_date: datetime = fake.date_this_year()
+       
+
+
+        django_contacts.append(
+            Home(
+                username=username,
+                password=password,
+                email=email,
+                created_date=created_date,
+            )
+        )
+
+    if len(django_contacts) > 0:
+        Home.objects.bulk_create(django_contacts)
