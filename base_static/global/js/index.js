0001fd6dc44713bd1bdc01de6ec098a3348ffdbe
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-24 11:18:56 -0300

    creating search

--- base_static/global/js/index.js
+++ base_static/global/js/index.js
@@ -1,6 +1,13 @@
-require("dotenv").config();
-
-const apiGetResponse =
-    "https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=yourAPIKey";
-
-console.log(process.env.KEY);
+function onSubmitBook() {
+    require("dotenv").config();
+    const nameBook = document.querySelector("input#bookSearch").value;
+    const apiKey = process.env.API_BOOK_KEY;
+    const apiGetResponse =
+        "https://www.googleapis.com/books/v1/volumes?q=" +
+        nameBook +
+        "&key=" +
+        apiKey;
+    fetch(apiGetResponse).then((response) => {
+        console.log(response);
+    });
+}
