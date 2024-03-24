c2ac603dd2e846cabc7983df11b277790e1bd24b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-24 14:34:15 -0300

    function active fetch

--- home/static/home/js/search.js
+++ home/static/home/js/search.js
@@ -1,17 +1,26 @@
 const nameBook = document.querySelector("span#bookValue").outerText;
-const apiGetEndpoint =
-    "https://www.googleapis.com/books/v1/volumes?q=" +
-    nameBook +
-    "&key=AIzaSyCsk64sineil4IZ_Mj80b0cMxPOm0T-r7w";
+const apiGetEndpoint = `https://www.googleapis.com/books/v1/volumes?q=${nameBook}&key=AIzaSyCsk64sineil4IZ_Mj80b0cMxPOm0T-r7w`;
 
 fetch(apiGetEndpoint)
-    .then((response) => {
-        console.log(response);
-        console.log(response.json());
-        response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
         console.log(data);
         const show = document.querySelector("div#showBooksFetch");
-        show.innerHTML = data;
+        let array = 1000;
+        for (let index = 0; index < array; index++) {
+            show.innerHTML += `
+            <div class="col-sm-3 text-center bg-light pt-3 my-2 mx-3 rounded">
+                <img width="90" class="img-fluid" src="${data.items[index].volumeInfo.imageLinks.thumbnail}" alt="img">
+                <p class="lead">
+                    ${data.items[index].volumeInfo.title}
+                </p>
+            </div>`;
+        }
+        // show.innerHTML = `
+        //     <div class="col-md-2 text-center bg-light pt-3 rounded">
+        //         <img class="img-fluid" src="${data.items[0].volumeInfo.imageLinks.thumbnail}" alt="img">
+        //         <p class="fs-5">
+        //             ${data.items[0].volumeInfo.title}
+        //         </p>
+        //     </div>`;
     });
