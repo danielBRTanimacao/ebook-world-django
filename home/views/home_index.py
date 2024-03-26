915e56be0fe1ac4d6e8a9d643fdcac7e85a1e976
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-26 10:50:27 -0300

    books search and title config

--- home/views/home_index.py
+++ home/views/home_index.py
@@ -6,24 +6,30 @@ def index(request):
     return render(request, 'home/index.html')
 
 def login(request):
-    return render(request, 'home/login.html')
+    context = {
+        'site_title': "Login - "
+    }
+    return render(request, 'home/login.html', context)
 
 def cadastro(request):
-    return render(request, 'home/cadastro.html')
+    context = {
+        'site_title': "Registrar - "
+    }
+    return render(request, 'home/cadastro.html', context)
 
 def user(request, url_id):
     user_single = get_object_or_404(Home, pk=url_id)
-
     context = {
         'home': user_single,
+        'site_title': f"{user_single.username} - ",
     }
-
     return render(request, 'home/user.html', context)
 
 def search_page(request):
     search_query = request.GET.get('q')
     context = {
         'query': search_query,
+        'site_title': f"{search_query} - ",
     }
     return render(request, 'home/search.html', context)
 
@@ -34,8 +40,11 @@ def specific_book(request, id_book):
             self.name = "nome"
             self.img_book = "img"
             self.description = "description"
-        
+    
+    search_book = Book()
+
     context = {
-        "book_id": Book,
+        "book_id": search_book,
+        'site_title': f"{search_book.name} - "
     }
     return render(request, 'home/specific_book.html', context)
