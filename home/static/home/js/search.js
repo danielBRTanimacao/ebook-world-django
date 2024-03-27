35393a4832ee2a646dc8a6ed0d25355d2c2a024e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-27 12:09:45 -0300

    creating area clicker to add book

--- home/static/home/js/search.js
+++ home/static/home/js/search.js
@@ -1,6 +1,8 @@
 const nameBook = document.querySelector("span#bookValue").outerText;
 const key = "AIzaSyCsk64sineil4IZ_Mj80b0cMxPOm0T-r7w";
 const apiGetEndpoint = `https://www.googleapis.com/books/v1/volumes?q=${nameBook}&key=${key}`;
+const erroImg =
+    "https://i.pinimg.com/564x/a2/9f/64/a29f6458abfb132920fb8a99d8b60f85.jpg";
 
 fetch(apiGetEndpoint)
     .then((response) => response.json())
@@ -11,21 +13,72 @@ fetch(apiGetEndpoint)
         let array = data.items.length;
         for (let index = 0; index < array; index++) {
             try {
+                let book_name = data.items[index].volumeInfo.title;
+                let book_img =
+                    data.items[index].volumeInfo.imageLinks.thumbnail;
+                let author = data.items[index].volumeInfo.authors;
+                let description = data.items[index].volumeInfo.description;
+                let categories = data.items[index].volumeInfo.categories;
                 show.innerHTML += `
-                    <a href="#" class="col-sm-3 text-center bg-light pt-3 my-2 mx-3 rounded text-dark text-decoration-none">
-                        <img class="img-fluid" src="${data.items[index].volumeInfo.imageLinks.thumbnail}" alt="img">
+                    <button 
+                        class="col-sm-3 text-center bg-light pt-3 my-2 mx-3 rounded btn"
+                        data-bs-toggle="modal" 
+                        data-bs-target="#exampleModal"
+                        onclick="setBook('${book_img}', '${book_name}', '${categories}', '${author}', '${description}')"
+                    >
+                        <img class="img-fluid" src="${book_img}" alt="img">
                         <p class="lead">
-                            ${data.items[index].volumeInfo.title}
+                            ${book_name}
                         </p>
-                    </a>`;
+                    </button>`;
             } catch (error) {
+                let book_name = data.items[index].volumeInfo.title;
+                let author = data.items[index].volumeInfo.authors;
+                let description = data.items[index].volumeInfo.description;
+                let categories = data.items[index].volumeInfo.categories;
                 show.innerHTML += `
-                    <a href="#" class="col-sm-3 text-center bg-light pt-3 my-2 mx-3 rounded text-dark text-decoration-none">
-                        <img class="img-fluid" width="180" src="https://i.pinimg.com/564x/a2/9f/64/a29f6458abfb132920fb8a99d8b60f85.jpg" alt="img-book">
+                    <button
+                        class="col-sm-3 text-center bg-light pt-3 my-2 mx-3 rounded btn"
+                        data-bs-toggle="modal" 
+                        data-bs-target="#exampleModal"
+                        onclick="setBook('${erroImg}', '${book_name}', '${categories}', '${author}', '${description}')"
+                    >
+                        <img class="img-fluid" width="180" src="${erroImg}" alt="img-book">
                         <p class="lead">
-                            ${data.items[index].volumeInfo.title}
+                            ${book_name}
                         </p>
-                    </a>`;
+                    </button>`;
             }
         }
     });
+
+function setBook(
+    img_path,
+    name_book,
+    categorie,
+    author = "autor não informado",
+    description = "descrição não informada"
+) {
+    const text_set = document.querySelector("div#modal-body-text");
+    text_set.innerHTML = `
+        <div class="col-md-4" >
+            <img class="img-fluid" width="200" src="${img_path}">
+        </div>
+        <div class="col-md-8" >
+            <h3>
+                ${name_book}
+            </h3>
+            <div>
+                <p>
+                    ${author}
+                </p>
+                <p>
+                    ${categorie}
+                </p>
+                <p>
+                    ${description}
+                </p>
+            </div>
+        </div>
+    `;
+}
