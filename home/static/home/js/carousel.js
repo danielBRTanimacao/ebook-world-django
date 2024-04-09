79f0a0cfa60c76c7afc8866839781bd6256912d7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-09 10:41:14 -0300

    rounded img

--- home/static/home/js/carousel.js
+++ home/static/home/js/carousel.js
@@ -35,7 +35,7 @@ function onScreenMinus() {
         text.classList.add("col-lg-6");
         text.innerHTML = `
             <img 
-            class="img-fluid"
+            class="img-fluid rounded"
             src="https://images.unsplash.com/photo-1512820790803-83ca734da794?q=80&w=1798&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="img-teste"
             >
