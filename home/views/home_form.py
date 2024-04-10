5ece136ef992fd0b87fcb7da49da081878bea11f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-10 18:37:37 -0300

    creating form using modelform

--- home/views/home_form.py
+++ home/views/home_form.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render
+from django import forms
 
 from home.models import Home
 
+class HomeForm(forms.ModelForm):
+    class Meta:
+        model = Home
+        fields = (
+            'username',
+            'email',
+            'password',
+        )
+
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'site_title': "Registrar - ",
+            'form': HomeForm(request.POST),
+        }
+        return render(request, 'home/create.html', context)
+        
     context = {
-        'site_title': "Registrar - "
+        'site_title': "Registrar - ",
+        'form': HomeForm(),
     }
     return render(request, 'home/create.html', context)
