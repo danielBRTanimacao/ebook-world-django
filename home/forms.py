565371d1e568a0cb4d9eddd0b0a2b0a892e7511c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-11 11:07:26 -0300

    new form removing new comnents

--- home/forms.py
+++ home/forms.py
@@ -4,6 +4,36 @@ from django.core.exceptions import ValidationError
 from home.models import Home
 
 class HomeForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Nome usuario...',
+            }
+        ),
+        label="Digite seu nome de usuario"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'id':'passwordInput',
+                'placeholder':'Sua senha...',
+            }
+        ),
+        help_text='Digite uma senha com 8 ou mais caracteres',
+        label="Digite uma senha"
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Email...',
+            }
+        ),
+        label="Digite um email valido"
+    )
+
     class Meta:
         model = Home
         fields = (
