90ec775e810d093989db4a105250caa92a745299
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-22 11:07:54 -0300

    Trying to validate user to enter in your account

--- home/views/home_form.py
+++ home/views/home_form.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from home.forms import HomeForm, LoginHomeForm
+from home.models import Home
 
 def create(request):
     form_action = reverse('home:cadastro')
@@ -26,12 +27,13 @@ def create(request):
     }
     return render(request, 'home/create.html', context)
 
-def login(request):
+def login(request, url_id):
     #Terminar e concerta o login
-    form_action = reverse('home:login')
+    home_user = get_object_or_404(Home, contact_id=url_id)
+    form_action = reverse('home:login', args=(url_id,))
 
     if request.method == 'POST':
-        form = LoginHomeForm(request.POST)
+        form = LoginHomeForm(request.POST, instance=home_user)
         context = {
             'site_title': "Login - ",
             'form': form,
@@ -40,9 +42,9 @@ def login(request):
 
         if form.is_valid():
             # validadar entrada de user
-            home_id = form
+            home_id = form.save()
             # fields pode coletar os nomes e validar o database
-            return redirect('home:user', url_id=home_id)
+            return redirect('home:user', url_id=home_id.pk)
         
         return render(request, 'home/login.html', context)
         
