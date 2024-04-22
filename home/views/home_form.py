ad9dc7a5b3a94685b93635cfc6cf5c2a26b93aca
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-22 12:16:49 -0300

    trying to make auth the user

--- home/views/home_form.py
+++ home/views/home_form.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from home.forms import HomeForm, LoginHomeForm
-from home.models import Home
 
 def create(request):
     form_action = reverse('home:cadastro')
@@ -27,13 +26,12 @@ def create(request):
     }
     return render(request, 'home/create.html', context)
 
-def login(request, url_id):
+def login(request):
     #Terminar e concerta o login
-    home_user = get_object_or_404(Home, contact_id=url_id)
-    form_action = reverse('home:login', args=(url_id,))
+    form_action = reverse('home:login')
 
     if request.method == 'POST':
-        form = LoginHomeForm(request.POST, instance=home_user)
+        form = LoginHomeForm(request.POST)
         context = {
             'site_title': "Login - ",
             'form': form,
@@ -41,10 +39,8 @@ def login(request, url_id):
         }
 
         if form.is_valid():
-            # validadar entrada de user
-            home_id = form.save()
-            # fields pode coletar os nomes e validar o database
-            return redirect('home:user', url_id=home_id.pk)
+            home_validated = form
+            return redirect('home:user', url_id=home_validated.pk)
         
         return render(request, 'home/login.html', context)
         
