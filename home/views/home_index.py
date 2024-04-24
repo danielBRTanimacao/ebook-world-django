12aa57b6974cedc81e02fe2f09260131c591a570
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-24 09:19:22 -0300

    account created and manage permissions

--- home/views/home_index.py
+++ home/views/home_index.py
@@ -15,13 +15,14 @@ def user(request, url_id, name_person):
     }
     return render(request, 'home/user.html', context)
 
+# Criar permissão para este usuario
 def account(request, url_id):
     user_single = get_object_or_404(Home, pk=url_id)
     context = {
         'home': user_single,
         'site_title': f"{user_single.username} - ",
     }
-    return render(request, 'home/user.html', context)
+    return render(request, 'home/account.html', context)
 
 def search_page(request):
     search_query = request.GET.get('q', '').strip()
