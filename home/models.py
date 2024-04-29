97c0027d0a2c64675f644415d3199d7fb12639ef
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-04-29 17:19:30 -0300

    making small configs

--- home/models.py
+++ home/models.py
@@ -6,7 +6,8 @@ class Home(models.Model):
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=20)
     created_date = models.DateTimeField(default=timezone.now)
-    # picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    description = models.TextField(blank=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     all_books = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     all_pages = models.DecimalField(max_digits=10, decimal_places=0, default=0)
 
