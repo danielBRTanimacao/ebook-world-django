8e6a74817eff8fa62354a65b2e20a1784de2d9d2
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-10 09:44:37 -0300

    creating new models

--- home/models.py
+++ home/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import User
 class UsersInfos(models.Model):
     description = models.TextField(blank=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    total_pages_read = models.CharField(max_length=7, default="0")
+    total_books = models.CharField(max_length=7, default="0")
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
