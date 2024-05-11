f123c0f42622d2f2307161ff9ec70f5802cb821d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-11 10:59:11 -0300

    new model post and testings

--- home/models.py
+++ home/models.py
@@ -9,3 +9,14 @@ class UsersInfos(models.Model):
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
 
+class Post(models.Model):
+    simple_text = models.CharField(max_length=200)
+    date_posted = models.DateField(auto_now=True)
+    post_picture = models.ImageField(blank=True, upload_to='posts/%Y/%m/')
+    autor_post = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.simple_text[0:100]
