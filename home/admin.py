a5ad7e7ca5395e886255066203ae90a277ce1545
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-11 11:29:17 -0300

    first post test

--- home/admin.py
+++ home/admin.py
@@ -8,3 +8,24 @@ class UsersInfosAdmin(admin.ModelAdmin):
         'id',
         'owner',
     )
+    ordering = '-id',
+    search_fields = (
+        'id',
+        'username',
+    )
+    list_per_page = 10
+    list_max_show_all = 150
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'autor_post',
+    )
+    ordering = '-id',
+    search_fields = (
+        'id',
+        'autor_post',
+    )
+    list_per_page = 10
+    list_max_show_all = 150
