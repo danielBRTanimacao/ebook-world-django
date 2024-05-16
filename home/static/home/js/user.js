389c18738466fe9a25aaebd77eaeaf3aa2cf7649
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-16 13:50:52 -0300

    making new changes and using js

---
+++ home/static/home/js/user.js
@@ -0,0 +1,6 @@
+function saveChangeHeart() {
+    let heart = document.querySelector("i.bi-heart");
+    heart.classList.remove("bi-heart");
+    heart.classList.add("bi-heart-fill", "text-danger");
+}
+// Making small testes
