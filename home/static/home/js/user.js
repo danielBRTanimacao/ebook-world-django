c9aa429fbc45b4660380dca3b7cd26ab9fb51405
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-16 13:55:46 -0300

    cancel js script

--- home/static/home/js/user.js
+++
@@ -1,6 +0,0 @@
-function saveChangeHeart() {
-    let heart = document.querySelector("i.bi-heart");
-    heart.classList.remove("bi-heart");
-    heart.classList.add("bi-heart-fill", "text-danger");
-}
-// Making small testes
