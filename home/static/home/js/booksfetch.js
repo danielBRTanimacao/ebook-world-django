d35d1482200cc3f3b8c545f16fc5ba037ccb9a41
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-17 16:48:46 -0300

    trying to clean code

--- home/static/home/js/booksfetch.js
+++ home/static/home/js/booksfetch.js
@@ -1,17 +1,18 @@
-const nameBook = "tecnologia";
-const key = "AIzaSyCsk64sineil4IZ_Mj80b0cMxPOm0T-r7w";
-const apiGetEndpoint = `https://www.googleapis.com/books/v1/volumes?q=${nameBook}&key=${key}`;
+function fetchBook(nameBook, nameRender) {
+    const key = "AIzaSyCsk64sineil4IZ_Mj80b0cMxPOm0T-r7w";
+    const apiGetEndpoint = `https://www.googleapis.com/books/v1/volumes?q=${nameBook}&key=${key}`;
 
-fetch(apiGetEndpoint)
-    .then((response) => response.json())
-    .then((data) => {
-        let lanc = document.querySelector("div#lanc");
-        array = data.items.length;
-        for (let index = 0; index < array; index++) {
-            try {
-                lanc.innerHTML += `<img class="img-fluid mx-1" src="${data.items[index].volumeInfo.imageLinks.thumbnail}" alt="book">`;
-            } catch (error) {
-                lanc.innerHTML += `<img class="img-fluid mx-1" src="https://source.unsplash.com/128x171/?book" alt="book">`;
+    fetch(apiGetEndpoint)
+        .then((response) => response.json())
+        .then((data) => {
+            let render = document.querySelector(`div#${nameRender}`);
+            array = data.items.length;
+            for (let index = 0; index < array; index++) {
+                try {
+                    render.innerHTML += `<img class="img-fluid mx-1" src="${data.items[index].volumeInfo.imageLinks.thumbnail}" alt="book">`;
+                } catch (error) {
+                    render.innerHTML += `<img class="img-fluid mx-1" src="https://source.unsplash.com/128x171/?book" alt="book">`;
+                }
             }
-        }
-    });
+        });
+}
